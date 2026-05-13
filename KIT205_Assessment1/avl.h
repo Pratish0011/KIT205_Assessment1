cbdab45737a7f3c66ba01b04f7f83570d6f745ad
Pratish <pratishbhandari2@gmail.com>
2026-05-13 23:17:55 +1000

    Defined AVL tree structures and function prototypes

--- KIT205_Assessment1/avl.h
+++ KIT205_Assessment1/avl.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef AVL_H
+#define AVL_H
+
+typedef struct AVLNode {
+    char key[20];
+    int height;
+    struct AVLNode* left;
+    struct AVLNode* right;
+} AVLNode;
+
+AVLNode* avl_insert(AVLNode* root, const char* key);
+void avl_print_inorder(AVLNode* root);
+void avl_free(AVLNode* root);
+int avl_contains(AVLNode* root, const char* key);
+
+#endif
