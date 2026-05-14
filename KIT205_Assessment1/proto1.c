42c78296c62ef452c911a5eddafaff5821af2cb2
Pratish <pratishbhandari2@gmail.com>
2026-05-14 17:58:57 +1000

    Add Proto1 print and free

--- KIT205_Assessment1/proto1.c
+++ KIT205_Assessment1/proto1.c
@@ -42,3 +42,58 @@ void p1_add_relation(P1Table* authors, P1Table* papers,
     author->relations = avl_insert(author->relations, paper_id);
     paper->relations = avl_insert(paper->relations, author_id);
 }
+
+void p1_print_all(P1Table* t) {
+    char ids[10000][20];
+    int count = 0;
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        P1Node* curr = t->buckets[i];
+        while (curr != NULL) {
+            strncpy(ids[count], curr->id, 19);
+            count++;
+            curr = curr->next;
+        }
+    }
+
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - i - 1; j++) {
+            if (strcmp(ids[j], ids[j + 1]) > 0) {
+                char tmp[20];
+                strcpy(tmp, ids[j]);
+                strcpy(ids[j], ids[j + 1]);
+                strcpy(ids[j + 1], tmp);
+            }
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("  %s\n", ids[i]);
+    }
+}
+
+void p1_print_relations(P1Table* t, const char* id) {
+    int idx = hash(id);
+    P1Node* curr = t->buckets[idx];
+    while (curr != NULL) {
+        if (strcmp(curr->id, id) == 0) {
+            avl_print_inorder(curr->relations);
+            return;
+        }
+        curr = curr->next;
+    }
+    printf("  (not found)\n");
+}
+
+void p1_free(P1Table* t) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        P1Node* curr = t->buckets[i];
+        while (curr != NULL) {
+            P1Node* tmp = curr;
+            avl_free(curr->relations);
+            curr = curr->next;
+            free(tmp);
+        }
+    }
+    free(t);
+}
