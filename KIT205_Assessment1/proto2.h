9b2f9878698985d8578f7be20d8d475c84e57d91
Pratish <pratishbhandari2@gmail.com>
2026-05-15 14:08:53 +1000

    Add Proto2 hash table structures

--- KIT205_Assessment1/proto2.h
+++ KIT205_Assessment1/proto2.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef PROTO2_H
+#define PROTO2_H
+
+#include "linkedlist.h"
+
+#define TABLE_SIZE 100
+
+typedef struct P2Node {
+    char id[20];
+    ListNode* relations;
+    struct P2Node* next;
+} P2Node;
+
+typedef struct {
+    P2Node* buckets[TABLE_SIZE];
+} P2Table;
+
+P2Table* p2_create();
+
+#endif
